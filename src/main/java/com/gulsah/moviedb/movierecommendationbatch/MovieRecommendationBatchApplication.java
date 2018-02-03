e7c84c5571d0310127ab021c4f985ef3d731bad9
gulsah <gulsahcoskunn@gmail.com>
2018-02-03 16:17:46 +0300

    Initial commit

---
+++ src/main/java/com/gulsah/moviedb/movierecommendationbatch/MovieRecommendationBatchApplication.java
@@ -0,0 +1,12 @@
+package com.gulsah.moviedb.movierecommendationbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommendationBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommendationBatchApplication.class, args);
+	}
+}
